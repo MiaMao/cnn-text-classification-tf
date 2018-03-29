abbfb9bcc0b84608eeaf6cd1514da0ada8bb2fa7
Mia <32235354+MiaMao@users.noreply.github.com>
2018-03-29 22:13:01 +0800

    Update data_helpers.py

--- data_helpers.py
+++ data_helpers.py
@@ -32,16 +32,24 @@ def load_data_and_labels(positive_data_file, negative_data_file):
     """
     # Load data from files
     positive_examples = list(open(positive_data_file, "r").readlines())
+    #strip 去掉前后的空格
     positive_examples = [s.strip() for s in positive_examples]
     negative_examples = list(open(negative_data_file, "r").readlines())
     negative_examples = [s.strip() for s in negative_examples]
     # Split by words
     x_text = positive_examples + negative_examples
     x_text = [clean_str(sent) for sent in x_text]
-    # Generate labels
+    #x_text[["a good.."],["mao..."]]
+    
+    # Generate 
+    #[[0,1],[0,1]...]
     positive_labels = [[0, 1] for _ in positive_examples]
     negative_labels = [[1, 0] for _ in negative_examples]
     y = np.concatenate([positive_labels, negative_labels], 0)
+     #array([[0, 1],
+    #   [0, 1],
+     #  [1, 0],
+      # [1, 0]])
     return [x_text, y]
 
 
